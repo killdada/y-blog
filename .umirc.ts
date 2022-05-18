7129307a2a82011ee2ed5c5be0a63ceee1dcacc9
yen <1124268308@qq.com>
2022-05-18 17:47:29 +0800

    docs: 新增netlify的构建命令，文件路径更改适配等

--- .umirc.ts
+++ .umirc.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'dumi';
 
+const isGithub = process.env.GITHUB === '1';
+
 export default defineConfig({
   title: 'yen blog',
   mode: 'site',
-  base: '/y-blog/',
-  publicPath: '/y-blog/',
+  base: isGithub ? '/y-blog/' : '/',
+  publicPath: isGithub ? '/y-blog/' : '/',
   // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   exportStatic: {},
   // more config: https://d.umijs.org/config
